b8b201f5666adb3da172f001c7e8f520705087f0
Kostiantyn <ksytnik73kwer@gmail.com>
2025-05-10 18:04:36 +0200

    Add Footer styles for Desktop

---
+++ versaepoch/src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import styles from '@/styles/footer.module.scss';
+import { NavElement } from '@/components/ui/NavElement';
+import { NavDropdown } from '@/components/ui/NavDropdown';
+import { GitHubButton } from './GitHubButton';
+
+export function Footer() {
+  return (
+    <div className={styles.container}>
+      {/* Navigation Section */}
+      <div className={styles.sections}>
+        {/* Other Section */}
+        <div className={styles.otherSection}>
+          <div className={styles.sectionElements}>
+            <span className={styles.slogan}>
+              The only wealth that expands when shared is{' '}
+              <strong>KNOWLEDGE</strong>
+            </span>
+            <GitHubButton />
+          </div>
+        </div>
+
+        <div className={styles.section}>
+          <h4 className={styles.sectionLabel}>Navigation</h4>
+          <div className={styles.sectionElements}>
+            <NavDropdown
+              title="Timelines"
+              elements={[
+                { title: 'ChatGPT', href: '/chatgpt' },
+                { title: 'Claude', href: '/claude' },
+                { title: 'Gemini', href: '/gemini' },
+              ]}
+            />
+            <NavElement name="LLMs Directory" href="llms-directory" />
+            <NavElement name="Prompts Directory" href="prompts-directory" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        Built with passion & 🧡 by Kostiantyn Sytnyk || All Rights Reserved 2025 ©
+      </div>
+    </div>
+  );
+}
