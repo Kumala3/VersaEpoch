f11b3150318a003c8c63f9fdf4fdf70883219bf6
Kostiantyn <ksytnik73kwer@gmail.com>
2025-05-29 18:27:11 +0200

    Add vscode to gitignore, deleted timelineCards

--- versaepoch/src/components/SourceElem.tsx
+++ versaepoch/src/components/SourceElem.tsx
@@ -1,15 +1,11 @@
-import Link from "next/link";
-import styles from "@/styles/sourceElem.module.scss";
+import Link from 'next/link';
+import styles from '@/styles/sourceElem.module.scss';
+import { Source } from '@/data/chatgptData';
 
-interface SourceElemProps{
-    title: string;
-    url: string;
-}
-
-export function SourceElem({title, url}: SourceElemProps) {
-    return (
-        <Link href={url} className={styles.container}>
-            {title}
-        </Link>
-    )
+export function SourceElem({ title, url }: Source) {
+  return (
+    <Link href={url} className={styles.container}>
+      {title}
+    </Link>
+  );
 }
