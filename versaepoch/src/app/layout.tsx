a3f06dcde55448f6a7c678c0920497e564ef9335
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2025-06-02 18:38:11 +0200

    Add Vercel Speed Insights (#14)

--- versaepoch/src/app/layout.tsx
+++ versaepoch/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import VercelAnalyticsClient from "@/components/AnalyticsClient";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Header />
         {children}
         <VercelAnalyticsClient />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
