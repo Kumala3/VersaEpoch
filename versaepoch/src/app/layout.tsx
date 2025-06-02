c45156c0447f90e71baaf632e5a6acfe8dd364b7
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2025-06-02 17:19:21 +0200

    Add vercel analytics (#12)

--- versaepoch/src/app/layout.tsx
+++ versaepoch/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
