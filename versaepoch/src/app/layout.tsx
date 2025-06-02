d2fa7e347d0edbefbd91172d8e6af412a281cc77
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2025-06-02 18:18:03 +0200

    Refactor analytics integration and improve timeline sorting logic (#13)

--- versaepoch/src/app/layout.tsx
+++ versaepoch/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/next"
+import VercelAnalyticsClient from "@/components/AnalyticsClient";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <Analytics />
+        <VercelAnalyticsClient />
         <Footer />
       </body>
     </html>
