4ff172aebf8e6800009e380f1401ff782e20d963
Kostiantyn <ksytnik73kwer@gmail.com>
2025-06-07 18:49:05 +0200

    Improved modalities column

--- versaepoch/src/components/ui/UIIcons.tsx
+++ versaepoch/src/components/ui/UIIcons.tsx
@@ -211,53 +211,109 @@ function MetaIcon({ width = 48, height = 48, className }: IconProps) {
   );
 }
 
-function ModalityTextIcon({ width = 48, height = 48, className }: IconProps) {
+function ModalityTextIcon({
+  width = 48,
+  height = 48,
+  className,
+  color = '#fff',
+}: IconProps) {
   return (
-    <Image
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      src={'/modality-text-icon.svg'}
-      alt="OpenAI Icon"
-      className={className}
-    />
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className}>
+      <path d="M15 12h6" />
+      <path d="M15 6h6" />
+      <path d="m3 13 3.553-7.724a.5.5 0 0 1 .894 0L11 13" />
+      <path d="M3 18h18" />
+      <path d="M3.92 11h6.16" />
+    </svg>
   );
 }
 
-function ModalityAudioIcon({ width = 48, height = 48, className }: IconProps) {
+function ModalityAudioIcon({
+  width = 48,
+  height = 48,
+  className,
+  color = '#fff',
+}: IconProps) {
   return (
-    <Image
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      src={'/modality-audio-icon.svg'}
-      alt="OpenAI Icon"
+      viewBox="0 0 24 24"
+      fill="none"
       className={className}
-    />
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <path d="M2 10v3" />
+      <path d="M6 6v11" />
+      <path d="M10 3v18" />
+      <path d="M14 8v7" />
+      <path d="M18 5v13" />
+      <path d="M22 10v3" />
+    </svg>
   );
 }
 
-function ModalityVideoIcon({ width = 48, height = 48, className, color = "white" }: IconProps) {
+function ModalityVideoIcon({
+  width = 48,
+  height = 48,
+  className,
+  color = '#fff',
+}: IconProps) {
   return (
-    <Image
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      src={'/modality-video-icon.svg'}
-      alt="OpenAI Icon"
-      quality={100}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
       className={className}
-      style={{stroke: "#000"}}
-    />
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <path d="M10 7.75a.75.75 0 0 1 1.142-.638l3.664 2.249a.75.75 0 0 1 0 1.278l-3.664 2.25a.75.75 0 0 1-1.142-.64z" />
+      <path d="M7 21h10" />
+      <rect width="20" height="14" x="2" y="3" rx="2" />
+    </svg>
   );
 }
 
-function ModalityImageIcon({ width = 48, height = 48, className }: IconProps) {
+function ModalityImageIcon({
+  width = 48,
+  height = 48,
+  className,
+  color = '#fff',
+}: IconProps) {
   return (
-    <Image
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      src={'/modality-image-icon.svg'}
-      alt="OpenAI Icon"
+      viewBox="0 0 24 24"
+      fill="none"
       className={className}
-    />
+      stroke={color}
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round">
+      <path d="M18 22H4a2 2 0 0 1-2-2V6" />
+      <path d="m22 13-1.296-1.296a2.41 2.41 0 0 0-3.408 0L11 18" />
+      <circle cx="12" cy="8" r="2" />
+      <rect width="16" height="16" x="6" y="2" rx="2" />
+    </svg>
   );
 }
 
