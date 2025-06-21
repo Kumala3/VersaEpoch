8eff013d03de1b9a58ba0236ecc9207f8c7c49ed
Kostiantyn <ksytnik73kwer@gmail.com>
2025-06-21 08:35:12 +0200

    Add toCamelCase function and update ResearchIcon with svg code

--- versaepoch/src/components/ui/TimelineCardsIcons.tsx
+++ versaepoch/src/components/ui/TimelineCardsIcons.tsx
@@ -4,6 +4,7 @@ interface IconProps {
   width?: number;
   height?: number;
   className?: string;
+  color?: string;
 }
 
 function ModelIcon({ width = 48, height = 48, className }: IconProps) {
@@ -30,15 +31,32 @@ function FeatureIcon({ width = 48, height = 48, className }: IconProps) {
   );
 }
 
-function ResearchIcon({ width = 48, height = 48, className }: IconProps) {
+function ResearchIcon({
+  width = 48,
+  height = 48,
+  className = '',
+  color = '#fff',
+}: IconProps) {
   return (
-    <Image
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      src={'/research-icon.svg'}
-      alt={'Research Icon'}
+      viewBox="0 0 24 24"
+      fill="none"
       className={className}
-    />
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      >
+      <path d="M6 18h8" />
+      <path d="M3 22h18" />
+      <path d="M14 22a7 7 0 1 0 0-14h-1" />
+      <path d="M9 14h2" />
+      <path d="M9 12a2 2 0 0 1-2-2V6h6v4a2 2 0 0 1-2 2Z" />
+      <path d="M12 6V3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3" />
+    </svg>
   );
 }
 
--- versaepoch/src/utils/helperFunctions.ts
+++ versaepoch/src/utils/helperFunctions.ts
@@ -11,3 +11,16 @@ export function capitalizeString(s: string, splitCharacter: string): string {
     .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
     .join(' ');
 }
+
+export function toCamelCase<T>(obj: T): T {
+  if (Array.isArray(obj)) {
+    return obj.map(toCamelCase) as T;
+  } else if (obj !== null && typeof obj === 'object') {
+    return Object.keys(obj).reduce((result, key) => {
+      const camelKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+      result[camelKey] = toCamelCase((obj as Record<string, unknown>)[key]);
+      return result;
+    }, {} as Record<string, unknown>) as T;
+  }
+  return obj;
+};
